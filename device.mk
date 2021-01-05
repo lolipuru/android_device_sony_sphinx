3c85e629a8b926d64f4fe6030aa60160bc91e963
LuK1337 <priv.luk@gmail.com>
2021-01-05 13:42:00 +0100

    sphinx: Build QTI bootctrl HAL
    Change-Id: I1e907c64ee4f88507bac78afe56e8fba0310f6d1

--- device.mk
+++ device.mk
@@ -43,6 +43,15 @@ AB_OTA_POSTINSTALL_CONFIG += \
 PRODUCT_PACKAGES += \
     otapreopt_script
 
+# Boot control
+PRODUCT_PACKAGES += \
+    android.hardware.boot@1.1-impl-qti \
+    android.hardware.boot@1.1-impl-qti.recovery \
+    android.hardware.boot@1.1-service
+
+PRODUCT_PACKAGES_DEBUG += \
+    bootctl
+
 # Rootdir
 PRODUCT_PACKAGES += \
     fstab.qcom \
