f953b2ba2292ab4dfa3db2d2b9a0a6a172d40b31
Alexander Koskovich <akoskovich@pm.me>
2022-04-12 05:00:05 -0700

    sphinx: Pick up USB configurations from QCOM
     * If we want to override anything in particular, e.g.
       PID/VIDs we will create a seperate script named
       'init.sony.usb.rc'.
    
    Change-Id: I83882d8992eb367211871130c163f4ca5689fcd4

--- device.mk
+++ device.mk
@@ -344,7 +344,12 @@ PRODUCT_PACKAGES += \
 
 # USB
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.3-service.dual_role_usb
+    android.hardware.usb@1.3-service.dual_role_usb \
+    init.qcom.usb.rc \
+    init.qcom.usb.sh
+
+PRODUCT_SOONG_NAMESPACES += \
+    vendor/qcom/opensource/usb/etc
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.usb.accessory.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.accessory.xml \
