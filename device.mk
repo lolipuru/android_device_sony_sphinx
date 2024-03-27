aaedb15819038ccb5e9c7dc88a45af320de42694
Yumi Yukimura <me.cafebabe@gmail.com>
2024-03-28 06:29:11 +0800

    sphinx: Use lineage radio config wrapper
    * This lets us have radio.config@1.1 interface while the modem is
      only capable of radio.config@1.0
    * Main purpose is to fix RIL after U QPR2, as radio.config@1.0 is
      no longer supported. Switching to radio.config@1.1 directly results
      in broken mobile data switch (Workaround that in telephony framework
      is possible, this way we fix it on AOSP GSIs too)
    
    Change-Id: I1dd299d45d92366005fe84c3275edded265d2da8

--- device.mk
+++ device.mk
@@ -187,6 +187,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.lights-service.sony_sphinx
 
+# Lineage interfaces
+PRODUCT_PACKAGES += \
+    framework_compatibility_matrix.lineage.xml
+
 # Media
 PRODUCT_PACKAGES += \
     libc2dcolorconvert \
@@ -249,6 +253,13 @@ PRODUCT_PACKAGES += \
     libprotobuf-cpp-full \
     librmnetctl \
     libxml2
+
+# RIL (Shim)
+PRODUCT_PACKAGES += \
+    android.hardware.radio.c_shim@1.0 \
+    android.hardware.radio.c_shim@1.1 \
+    android.hardware.radio.c_shim@1.2 \
+    android.hardware.radio.config@1.1-service.wrapper
 
 # Rootdir
 PRODUCT_PACKAGES += \
