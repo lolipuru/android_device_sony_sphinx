1c1ecbf02a57ff7d157ae00f3b47bd8f2cff2634
Bruno Martins <bgcngm@gmail.com>
2025-11-17 16:40:53 -0300

    sphinx: Use the common device compatibility matrix from QCOM
    Change-Id: I6600be0d9fca116482bb391b4fa66aec85db4f8e

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -186,6 +186,7 @@ BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 2
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
     $(DEVICE_PATH)/configs/vintf/framework_compatibility_matrix.xml \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml
+DEVICE_MATRIX_FILE := hardware/qcom-caf/common/compatibility_matrix.xml
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/configs/vintf/manifest.xml
 
 # WiFi
