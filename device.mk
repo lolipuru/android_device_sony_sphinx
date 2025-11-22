c67f97ad0fea6808798ab205c63cfd03b78ab36f
voidanix <voidanix@keyedlimepie.org>
2025-11-22 18:25:43 +0100

    sphinx: Add powerhint.json
    Kanged from oplus-sm8150 @ 1ddc851
    
    Change-Id: I9b1788ed9b1f85cfda507d24b54dc2416b289cc2

--- device.mk
+++ device.mk
@@ -254,6 +254,9 @@ PRODUCT_PACKAGES += \
     android.hardware.power-service.lineage-libperfmgr \
     libqti-perfd-client
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/perf/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
+
 # RIL
 PRODUCT_PACKAGES += \
     extphonelib \
