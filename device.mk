206c61ab23ec2d99116ea4cd06be2908bb71acc0
lolipuru <banstellazz@gmail.com>
2026-04-12 00:11:57 +0700

    sphinx: initial overlay
    - CarrierConfig : from 55.0.B.0.715
    - the rest are from https://git.keyedlimepie.org/voidanix/android_device_sony_sm8150-common/src/branch/lineage-23.0/overlay as sphinx don't ship overlay
    
    Change-Id: I1ddc88a8fafefc84f96cb70b6590df2d0f5613ee
    Signed-off-by: lolipuru <banstellazz@gmail.com>

--- device.mk
+++ device.mk
@@ -327,6 +327,15 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/com.android.nfc_extras.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.android.nfc_extras.xml \
     frameworks/native/data/etc/com.nxp.mifare.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.nxp.mifare.xml
 
+# Overlay
+PRODUCT_PACKAGES += \
+    CarrierConfigResSphinx \
+    FrameworksResSphinx \
+    NcmTetheringOverlay \
+    SystemUIResSpinx \
+    TelephonyResSphinx \
+    WifiResSphinx
+
 # Perf
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.perf@1.0.vendor
