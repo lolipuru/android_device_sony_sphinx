a995691afa60c6765771e88c5dc4c0a3df1d5249
lolipuru <banstellazz@gmail.com>
2026-05-09 04:29:52 +0700

    sphinx: migrate dt2w into powerhint
    Change-Id: Ic8fbae51c63241e3264f900c8487f09bcc58dec3
    Signed-off-by: lolipuru <banstellazz@gmail.com>

--- device.mk
+++ device.mk
@@ -256,8 +256,6 @@ PRODUCT_PACKAGES += \
     WifiResSphinx
 
 # Power
-$(call soong_config_set,qtipower,mode_ext_lib,//$(LOCAL_PATH):libpowermode-ext-sony)
-
 PRODUCT_PACKAGES += \
     android.hardware.power-service.lineage-libperfmgr \
     libqti-perfd-client
